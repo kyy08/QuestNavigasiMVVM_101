b777bb471227becafc64267fff4ca7b4b60fc3fd
kyy08 <kyyelzz1@gmail.com>
2024-11-21 21:49:40 +0700

    membuat object data jenis kelamin di data

---
+++ app/src/main/java/com/example/questnavigasimvvm_101/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasimvvm_101.model
+
+import com.example.questnavigasimvvm_101.R
+
+object JenisKelamin {
+    val jeniskelamin = listOf(
+        R.string.laki,
+        R.string.wanita,
+    )
+}
