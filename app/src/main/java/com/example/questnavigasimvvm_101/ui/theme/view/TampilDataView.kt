95b3c0b47f3f93e9b3c21fa771b9ed7b6a6dbaa6
kyy08 <kyyelzz1@gmail.com>
2024-11-21 22:01:56 +0700

    membuat fungsi TampilData

--- app/src/main/java/com/example/questnavigasimvvm_101/ui/theme/view/TampilDataView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_101/ui/theme/view/TampilDataView.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pam_5.Model.DataSiswa
+import com.example.questnavigasimvvm_101.model.DataSiswa
 
 //@Preview(showBackground = true)
 @Composable
@@ -29,6 +29,20 @@ fun TampilDataView(modifier: Modifier = Modifier,
         TampilData(parameterName = "No Telepon", argu = uiState.notelepon)
         Button(onClick = onBackButton){
             Text(text = "Kembali")
+        }
+    }
+}
+
+@Composable
+fun TampilData(parameterName : String, argu : String) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = parameterName, modifier = Modifier.weight(0.8f))
+            Text(text = ":", modifier = Modifier.weight(0.2f))
+            Text(text = argu, modifier = Modifier.weight(2f))
         }
     }
 }
