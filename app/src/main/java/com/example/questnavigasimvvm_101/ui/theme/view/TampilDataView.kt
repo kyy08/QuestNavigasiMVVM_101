2a2eba23713c571a5bf8326ecf29c36f410c6056
kyy08 <kyyelzz1@gmail.com>
2024-11-21 21:59:49 +0700

    membuat fungsi TampilDataView

--- app/src/main/java/com/example/questnavigasimvvm_101/ui/theme/view/TampilDataView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_101/ui/theme/view/TampilDataView.kt
@@ -1,36 +1,34 @@
-package com.example.questnavigasimvvm_101.ui.theme.view
-
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.pam_5.Model.DataSiswa
 
 //@Preview(showBackground = true)
 @Composable
-fun FormulirView(modifier: Modifier = Modifier, listJk: List<String>,
-                 onSubmitClicked: (MutableList<String>) -> Unit) {
-    var nama  by remember { mutableStateOf("") }
-    var email  by remember { mutableStateOf("") }
-    var alamat by remember { mutableStateOf("") }
-    var notelepon by remember { mutableStateOf("") }
-    var gender by remember { mutableStateOf("") }
+fun TampilDataView(modifier: Modifier = Modifier,
+                   uiState: DataSiswa,
+                   onBackButton: () -> Unit
 
-    val listData: MutableList<String> = mutableListOf(nama,gender,alamat,email,notelepon)
+) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+        TampilData(parameterName = "Nama", argu = uiState.nama)
+        TampilData(parameterName = "Jenis kelamin", argu = uiState.gender)
+        TampilData(parameterName = "Alamat", argu = uiState.alamat)
+        TampilData(parameterName = "Email", argu = uiState.email)
+        TampilData(parameterName = "No Telepon", argu = uiState.notelepon)
+        Button(onClick = onBackButton){
+            Text(text = "Kembali")
+        }
+    }
+}
