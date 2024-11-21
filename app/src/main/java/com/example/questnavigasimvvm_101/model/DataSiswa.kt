ec07a6f3a7f134a32e95212fdaf11c69c1d3a6df
kyy08 <kyyelzz1@gmail.com>
2024-11-21 21:41:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/questnavigasimvvm_101/model/DataSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.questnavigasimvvm_101.model
+
+data class DataSiswa(
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val email: String = "",
+    val notelepon: String = ""
+)
